3628f943582fdee60a6d468fa818ff1c4557df14
Leon Lamle <llamle@gmail.com>
2015-08-04 10:46:07 -0400

    added validations to models

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,8 +4,8 @@ class SessionsController < ApplicationController
   end
 
   def create
-        
-    user = User.find_by_email(params[:email])
+    # Allows users to log in with either their email or username
+    user = User.find_by(email: params[:email] || username: params[:username])
     # If the user exists AND the password entered is correct.
     if user && user.authenticate(params[:password])
       # Save the user id inside the browser cookie. This is how we keep the user
@@ -13,14 +13,21 @@ class SessionsController < ApplicationController
       session[:user_id] = user.id
       redirect_to '/'
     else
-    # If user's login doesn't work, send them back to the login form.
-      redirect_to '/login'
+    # If user's login doesn't work, flash error and redirect them back to the login form.
+    flash[:message] = "Username or Password error."
+    redirect_to new_user_path
     end
   end
 
   def destroy
+    # Sets session to nil, then redirects to main page
     session[:user_id] = nil
-    redirect_to '/login'
+    redirect_to new_user_path
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
   end
 
 end
