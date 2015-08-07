1d7f3a19c43808e386c9727efe551874ba402d39
Leon Lamle <llamle@gmail.com>
2015-08-07 09:56:58 -0400

    heroku push

--- app/assets/javascripts/angular.js
+++
@@ -1,9 +0,0 @@
-var app = angular.module('PerfectRez', []);
-
-app.controller('PerfectRezCtrl', ['$http', function($http){
-  this.formData = '';
-  currentThis = this;
-  this.formAction = function ( {
-    $http.get('')
-  })
-}])
