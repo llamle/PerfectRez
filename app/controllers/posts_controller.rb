20dc37966c4b0424877e0330053d6aabfb0fa9bc
Leon Lamle <llamle@gmail.com>
2015-08-10 13:44:16 -0400

    fixing twilio

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy]
+  skip_before_filter  :verify_authenticity_token, only: :create
 
   # GET /posts
   # GET /posts.json
